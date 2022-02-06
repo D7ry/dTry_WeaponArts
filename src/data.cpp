03b23e83615fbf4a5b0be53f6ed5f2a0e448f45c
d7ry <92609548+D7ry@users.noreply.github.com>
2022-02-06 05:10:17 -0800

    support "-" character

--- src/data.cpp
+++ src/data.cpp
@@ -33,7 +33,7 @@ a sample line: hitframe|0x00012EB7|Skyrim.esm|30|20|1|
 @param a_name the name of weapon art, as the key in the ini that's being passed in*/
 bool dataHandler::readLine(std::string a_line, std::string a_name) {
 	INFO("Weapon art {}, config line: {}", a_name, a_line);
-	if (!std::regex_match(a_line, std::regex("((\\w)+\\|){2}((\\w|\\.|\\s)+\\|)((\\d)+\\|){5}(\\d)+"))) {
+	if (!std::regex_match(a_line, std::regex("((\\w)+\\|){2}((\\w|\\.|\\s|\\-)+\\|)((\\d)+\\|){5}(\\d)+"))) {
 		INFO("Error: wrong config line format; failed to read this weapon art.");
 		return false;
 	}
